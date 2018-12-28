59a72c5f75ec18745c37c77f95e1992b5379aeb8
sewon-swjh0121 <swjh0121@gmail.com>
2018-12-29 02:27:10 +0900

    Add function(Cal)

---
+++ cal.py
@@ -0,0 +1,26 @@
+#Control Data
+
+class Calculator:
+
+    def __init__(self):
+        self.result = 0
+
+    def sum_num(self,num1,num2):
+        self.result = num1 + num2
+        return self.result
+
+    def minus_num(self,num1,num2):
+        self.result = num1 - num2
+        return self.result
+
+    def multi_num(self,num1,num2):
+        self.result = num1 * num2
+        return self.result
+
+    def div_num(self,num1,num2):
+        self.result = num1 / num2
+        return self.result
+
+    def pow_num(self,num1,num2):
+        self.result = num1 ** num2
+        return self.result
