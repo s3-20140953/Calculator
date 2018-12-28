fc42ef6a8d136302e57bbff755bae5c8228e6a80
sewon-swjh0121 <swjh0121@gmail.com>
2018-12-29 02:24:12 +0900

    Fix structure

---
+++ setdata.py
@@ -0,0 +1,11 @@
+#set Data
+
+class SetData:
+
+    def __init__(self):
+        self.num1 = 0
+        self.num2 = 0
+
+    def set_data(self,num1,nusm2):
+        self.num1 = num1s
+        self.num2 = num2
